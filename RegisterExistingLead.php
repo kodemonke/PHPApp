fb2154f27b37da0bf4c28608dfac3f732f8eabf6
Adam Andrews <adam@legitster.com>
2016-12-21 14:15:45 -0800

    more fixes

--- RegisterExistingLead.php
+++ RegisterExistingLead.php
@@ -16,19 +16,19 @@ if (ctype_digit($mktoLead) && strlen($mktoLead) < 10 && strlen($mktoLead) > 7 &&
 				"programID"=>$mktoProgram,
 				"id"=>$mktoLead
 				);
-				print_r($payload);
-					
-				$repackData = array_push($memberList,$payload);
-				
-				print_r($repackData);
-				
-				file_put_contents($directory.'/programs/inProcess.json',json_encode($repackData));
-				
-				echo "Success!";
-				
 				}	
-			else{echo "Not in program";}
-		}		
+		}
+		if (empty($payload)){echo "Not in Program.";}
+		else {
+			echo "Success!";
+			print_r($payload);
+					
+			$repackData = array_push($memberList,$payload);
+			
+			print_r($repackData);
+			
+			file_put_contents($directory.'/programs/inProcess.json',json_encode($repackData));
+		}
 	}
 }
 else{echo "Invalid Input";}
