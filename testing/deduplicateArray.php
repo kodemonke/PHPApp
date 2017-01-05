ec0314806de52f93a7cdc499bb287b4ac9f3c723
Sean Ryan <s-ryan78@live.com>
2017-01-05 10:49:38 -0800

    once again

--- testing/deduplicateArray.php
+++
@@ -1,8 +0,0 @@
-<?php
-$Leads = json_decode('[{"programID":"7226","id":"10354285"},{"programID":"7226","id":"10354285"},{"programID":"7236","id":"10354285"},{"programID":"7226","id":"1035885"}]');
-
-$junk = array_map("unserialize", array_unique(array_map("serialize", $Leads)));
-echo "<br/>";
-echo json_encode($junk);
-
-?>
